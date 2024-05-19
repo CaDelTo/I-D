21bad17183cef1e823b25ea61818dc546ef3965d
CaDelTo <cadeltoro@uninorte.edu.co>
2024-05-19 02:10:20 -0500

    refactor: modify scan_ports function to accept a list of ports instead of start and end port range

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import socket
 
 import concurrent.futures
 
-def scan_ports(target, start_port, end_port):
+def scan_ports(target, ports):
     open_ports = []
     closed_ports = []
     filtered_ports = []
@@ -24,17 +24,34 @@ def scan_ports(target, start_port, end_port):
         sock.close()
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(scan_port, range(start_port, end_port + 1))
+        executor.map(scan_port, ports)
 
     return open_ports, closed_ports, filtered_ports
 
 # Example usage
-target_ip = "192.168.1.1"
-start_port = 1
-end_port = 65536
 
-open_ports, closed_ports, filtered_ports = scan_ports(target_ip, start_port, end_port)
-#Opciociones quicksan, fullscan, entre rangos de puertos y puertos especificos
+
+commonPorts = [21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5900, 8080]
+
+ip = input('Enter target IP: ')
+opc = input('Select an option:\n1. Quickscan (Common Ports)\n2. Fullscan (Ports 1-65536)\n3.Range\n4.Specific Port\nEnter option:')
+if opc == '1':
+    ports = commonPorts
+elif opc == '2':
+    print('Scanning all ports (1-65536) This may take a while...')
+    ports = list(range(1, 65537))
+elif opc == '3':   
+    start_port = int(input('Enter start port: '))
+    end_port = int(input('Enter end port: '))
+    port = list(range(start_port, end_port+1))
+elif opc == '4':
+    start_port = int(input('Enter port: '))
+    ports = [start_port]
+else:
+    print('Invalid option')
+    exit()
+print('Scanning...')
+open_ports, closed_ports, filtered_ports = scan_ports(ip, ports)
 print("Open ports:", open_ports)
 print("Closed ports:", closed_ports)
 print("Filtered ports:", filtered_ports)
